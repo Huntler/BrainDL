7a5d51e4a2d6ce7857332e5e18820c972a14a485
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2022-05-12 20:25:42 +0200

    added debug option

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+
+import torch
 from model.brain_behaviour_classifier import BrainBehaviourClassifier
 from submodules.TimeSeriesDL.utils.config import config
 
@@ -7,11 +9,25 @@ from submodules.TimeSeriesDL.utils.config import config
 config.register_model("BrainBehaviourClassifier", BrainBehaviourClassifier)
 
 
+def testing():
+    # create a dummy input
+    # batch_size, sequence length, sample (248 values reshaped to 8x31)
+    x = torch.zeros((64, 4, 8, 31))
+
+    model = BrainBehaviourClassifier()
+    out = model(x)
+
+
 if __name__ == "__main__":    
     parser = argparse.ArgumentParser(description="This program trains and tests a deep " + 
                                      "learning model to detect a behaviour based on brain data")
     parser.add_argument("--config", dest="config", help="Set path to config file.")
+    parser.add_argument("--debug", dest="debug", help="Runs the model with a dummy input to debug it.", action="store_true")
     args = parser.parse_args()
+
+    if args.debug:
+        testing()
+        quit()
 
     if args.config:
         pass
