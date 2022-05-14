c293f03422fd254fbbfb198e3ecf84d10ca1f476
tomsabanov <tom.sabanov@gmail.com>
2022-05-14 15:40:24 +0200

    removoed self

--- data/utils.py
+++ data/utils.py
@@ -14,13 +14,13 @@ def get_file_label(self,file):
     else:
         return 3
 
-def __get_dataset_name(self,file_name_with_dir: str) -> str:
+def __get_dataset_name(file_name_with_dir: str) -> str:
     filename_without_dir = file_name_with_dir.split('/')[-1]
     temp = filename_without_dir.split('_')[:-1]
     dataset_name = "_".join(temp)
     return dataset_name
 
-def get_dataset_matrix(self,file_path: str) -> np.array:
+def get_dataset_matrix(file_path: str) -> np.array:
     with h5py.File(file_path,'r') as f:
         dataset_name = self.__get_dataset_name(file_path)
         matrix = f.get(dataset_name)
